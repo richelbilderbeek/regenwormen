4d20b28961dd73788608404b5b390604f159a670
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-02 13:41:11 +0100

    Always test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "game_dialog.h"
 
+#include <iostream>
 #include <QApplication>
 
 void test()
@@ -9,14 +10,17 @@ void test()
 
 int main(int argc, char *argv[])
 {
+  #ifndef NDEBUG
+  std::cout << "Debug mode\n";
+  test();
+  #else
+  std::cout << "Release mode\n";
+  assert(1 == 2); //No tests in release mode
+  #endif
+
+  if (argc == 2 && std::string(argv[1]) == "--test") return 0;
+
   QApplication a(argc, argv);
-
-  if (argc == 2 && std::string(argv[1]) == "--test")
-  {
-    test();
-    return 0;
-  }
-
   game_dialog w;
   w.show();
   return a.exec();
