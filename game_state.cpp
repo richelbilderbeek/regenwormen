268aa97cfc5a8650d4bf0d7cbf5b8104b2226922
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-02 19:09:31 +0100

    A game state has 2 players by default

--- game_state.cpp
+++ game_state.cpp
@@ -3,10 +3,19 @@
 #include <cassert>
 
 game_state::game_state(
-  const tiles& availabe_tiles
-) : m_availabe_tiles{availabe_tiles}
+  const tiles& available_tiles,
+  const tiles& unavailable_tiles,
+  const std::vector<tiles> player_tiles
+) : m_available_tiles{available_tiles},
+    m_player_tiles{player_tiles},
+    m_unavailable_tiles{unavailable_tiles}
 {
 
+}
+
+int get_n_players(const game_state& s) noexcept
+{
+  return s.get_player_tiles().size();
 }
 
 void test_game_state()
@@ -15,5 +24,11 @@ void test_game_state()
   {
     game_state g;
     assert(!g.get_available_tiles().empty());
+    assert(g.get_unavailable_tiles().empty());
+  }
+  // A state has two players by defau;t
+  {
+    game_state g;
+    assert(get_n_players(g) == 2);
   }
 }
--- game_state.h
+++ game_state.h
@@ -7,17 +7,39 @@
 class game_state
 {
 public:
+  /// Create a game state
+  /// @param available_tiles tiles that are still available
+  /// @param unavailable_tiles tiles that flipped, thus unavailable
+  /// @param player_tiles collections of tiles of each player. For examples,
+  ///   'player_tiles[0]' is the collection of tiles that player 1 has
   game_state(
-    const tiles& availabe_tiles = create_all_tiles()
+    const tiles& available_tiles = create_all_tiles(),
+    const tiles& unavailable_tiles = {},
+    const std::vector<tiles> player_tiles = {{}, {}}
   );
 
   /// Get the tiles that are still available
-  const auto& get_available_tiles() const noexcept { return m_availabe_tiles; }
+  const auto& get_available_tiles() const noexcept { return m_available_tiles; }
+
+  /// Get the tiles that the players have.
+  const auto& get_player_tiles() const noexcept { return m_player_tiles; }
+
+  /// Get the tiles that are flipped, thus unavailable
+  const auto& get_unavailable_tiles() const noexcept { return m_unavailable_tiles; }
 
 private:
   /// The tiles that are still available
-  tiles m_availabe_tiles;
+  tiles m_available_tiles;
+
+  /// The tiles of each player
+  std::vector<tiles> m_player_tiles;
+
+  /// The tiles that are flipped, thus unavailable
+  tiles m_unavailable_tiles;
 };
+
+/// Get the number of players
+int get_n_players(const game_state& s) noexcept;
 
 /// Test the game_state class
 void test_game_state();
